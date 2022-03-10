aa45a7e706d6347ca95e9baa0a0e16c0b97f009b
Marc Newlin <marc.newlin@gmail.com>
2022-03-09 22:43:09 -0800

    cors update to use coi-serviceworker.js

--- webapp/server.py
+++ webapp/server.py
@@ -8,8 +8,8 @@ def test (*args):
 
 class CORSRequestHandler (SimpleHTTPRequestHandler):
     def end_headers (self):
-        self.send_header('Cross-Origin-Embedder-Policy', 'require-corp')
-        self.send_header('Cross-Origin-Opener-Policy', 'same-origin')
+        # self.send_header('Cross-Origin-Embedder-Policy', 'require-corp')
+        # self.send_header('Cross-Origin-Opener-Policy', 'same-origin')
         SimpleHTTPRequestHandler.end_headers(self)
 
 if __name__ == '__main__':
